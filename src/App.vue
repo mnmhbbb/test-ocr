8d190896814fb59ef589a9f25203a0c3756c4b0b
mokaReadyEnt <mnmhbbb@readyent.com>
2024-04-04 18:06:07 +0900

    ✨ create function to extract text from images using OCR API

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input type="file" @change="processImage" accept="image/*" />
+    <p v-if="extractedText">추출된 텍스트: <br />{{ extractedText }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const extractedText = ref('');
+
+const processImage = async (event: Event) => {
+  const file = (event.target as HTMLInputElement).files?.[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const base64Image = reader.result?.toString().split(',')[1];
+      if (base64Image) {
+        sendToVisionApi(base64Image);
+      }
+    };
+    reader.readAsDataURL(file);
+  }
+};
+
+interface AnnotateImageResponse {
+  responses: {
+    textAnnotations: { description: string }[];
+  }[];
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+const sendToVisionApi = async (base64Image: string) => {
+  try {
+    const response = await axios.post<AnnotateImageResponse>(`/vision?key=${API_KEY}`,
+      {
+        requests: [
+          {
+            image: {
+              content: base64Image,
+            },
+            features: [
+              {
+                type: 'TEXT_DETECTION',
+              },
+            ],
+          },
+        ],
+      }
+    );
+    if (response.data.responses[0].textAnnotations) {
+      extractedText.value = response.data.responses[0].textAnnotations[0].description;
+    } else {
+      extractedText.value = '텍스트를 찾을 수 없습니다.';
+    }
+  } catch (error) {
+    alert('에러 발생')
+    console.error(error);
+  }
+};
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/vision': {
+        target: 'https://vision.googleapis.com/v1/images:annotate',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/vision/, ''),
+      },
+    }
+  },
 })
